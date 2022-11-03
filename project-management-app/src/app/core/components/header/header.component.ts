35373b08ff80f4aee5265a6a9d8167a29c873f56
Anna Tanisheva <anna-tanisheva@yandex.ru>
2022-11-03 19:01:21 +0300

    feat: add buttons to login, signup and logout to the header

--- project-management-app/src/app/core/components/header/header.component.ts
+++ project-management-app/src/app/core/components/header/header.component.ts
@@ -1,8 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { map, Subscription } from 'rxjs';
+
+import * as fromApp from '../../../store/app.reducer';
+import * as AuthActions from '../../../auth/store/auth.actions'
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit, OnDestroy  {
+  isAuthenticated = false;
+  private userSub: Subscription | undefined;
+
+  constructor(
+    private store: Store<fromApp.AppState>,
+    private router: Router,
+    private route: ActivatedRoute
+    ) {}
+
+  ngOnInit(): void {
+    this.userSub = this.store.select('auth').pipe(
+      map((authState) => {
+        return authState.user;
+      })
+    ).subscribe((user) => {
+      this.isAuthenticated = !!user;
+    })
+  }
+  ngOnDestroy(): void {
+    this.userSub?.unsubscribe();
+  }
+
+  onLogin(){
+    this.router.navigate(['/auth/login'])
+  }
+  onSignup(){
+    this.router.navigate(['/auth/signup'])
+  }
+  onLogout(){
+    this.store.dispatch(new AuthActions.Logout())
+  }
+}
