55ebad28e91931083b0fb31ce5768c16f4049c30
Andrei Smolich <andy.smolich@gmail.com>
2022-11-03 15:16:48 +0100

    feat: render projects action panel with dialog form

---
+++ project-management-app/src/app/projects/components/action-panel/action-panel.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ProjectFormComponent } from './project-form/project-form.component';
+
+@Component({
+  selector: 'app-action-panel',
+  templateUrl: './action-panel.component.html',
+  styleUrls: ['./action-panel.component.scss'],
+})
+export class ActionPanelComponent {
+  constructor(public dialog: MatDialog) {}
+
+  openDialog(): void {
+    this.dialog.open(ProjectFormComponent, {
+      autoFocus: true,
+    });
+  }
+}
--- project-management-app/src/app/projects/projects.module.ts
+++ project-management-app/src/app/projects/projects.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { ProjectsRoutingModule } from './projects-routing.module';
+import { ActionPanelComponent } from './components/action-panel/action-panel.component';
+import { SearchComponent } from './components/action-panel/search/search.component';
+import { ProjectFormComponent } from './components/action-panel/project-form/project-form.component';
 
 @NgModule({
-  declarations: [ProjectsComponent],
-  imports: [CommonModule, ProjectsRoutingModule],
+  declarations: [
+    ProjectsComponent,
+    ActionPanelComponent,
+    SearchComponent,
+    ProjectFormComponent,
+  ],
+  imports: [
+    CommonModule,
+    ProjectsRoutingModule,
+    MatIconModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
 })
 export class ProjectsModule {}
