800e7961beada3d8894c8122b3b50284f8401c2c
Andrei Smolich <andy.smolich@gmail.com>
2022-11-25 23:33:20 +0100

    feat: implement welcome page design

--- project-management-app/src/app/core/components/header/mobile-menu/mobile-menu.component.ts
+++ project-management-app/src/app/core/components/header/mobile-menu/mobile-menu.component.ts
@@ -1,22 +1,23 @@
-import { Component,Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import * as AuthActions from '../../../../auth/store/auth.actions';
 
 @Component({
   selector: 'app-mobile-menu',
   templateUrl: './mobile-menu.component.html',
-  styleUrls: ['./mobile-menu.component.scss']
+  styleUrls: ['./mobile-menu.component.scss'],
 })
 export class MobileMenuComponent implements OnInit {
   @Input() isAuthenticated!: boolean | null;
-  constructor( private store: Store) { }
 
-  ngOnInit(): void {
-  }
+  constructor(private store: Store) {}
+
+  ngOnInit(): void {}
 
   onClick() {
-    console.log(this.isAuthenticated)
+    console.log(this.isAuthenticated);
   }
+
   onLogout() {
     this.store.dispatch(AuthActions.logout({ isAutoLogout: false }));
   }
